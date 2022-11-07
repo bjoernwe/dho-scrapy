aa43fee19b7fcbd2a9f0d9f3397b25a96ba79585
Björn <bjoernwe@users.noreply.github.com>
2022-11-07 16:32:39 +0100

    Crawl all pages of category

--- src/scraper/spiders/DhOSpider.py
+++ src/scraper/spiders/DhOSpider.py
@@ -7,10 +7,15 @@ class DhOSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.dharmaoverground.org/discussion/-/message_boards/category/103268',  # Dharma Diagnostics
+            # Dharma Diagnostics
+            'https://www.dharmaoverground.org/discussion/-/message_boards/category/103268?_com_liferay_message_boards_web_portlet_MBPortlet_delta2=20&_com_liferay_message_boards_web_portlet_MBPortlet_orderByCol=modified-date&_com_liferay_message_boards_web_portlet_MBPortlet_orderByType=desc&_com_liferay_message_boards_web_portlet_MBPortlet_resetCur=false&_com_liferay_message_boards_web_portlet_MBPortlet_cur2=1',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response: HtmlResponse, **kwargs):
-        pass
+        next_page_urls = response.xpath('//ul[contains(@class, "pagination")]/li[not(contains(@class, "inactive"))]/a/@href').getall()
+        for next_page_url in next_page_urls:
+            if not next_page_url or not next_page_url.startswith('http'):
+                continue
+            yield scrapy.Request(next_page_url, callback=self.parse)
