bb4fd121a0304ac958212289827c76cef39536ea
Björn <bjoernwe@users.noreply.github.com>
2022-11-07 18:03:51 +0100

    Minimum message parsing

--- src/scraper/spiders/DhOSpider.py
+++ src/scraper/spiders/DhOSpider.py
@@ -1,7 +1,9 @@
 from typing import List
 
 import scrapy
-from scrapy.http import HtmlResponse
+
+from scrapy.http import HtmlResponse, XmlResponse
+from scraper.items import DhOMessage
 
 
 class DhOSpider(scrapy.Spider):
@@ -16,11 +18,29 @@ class DhOSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response: HtmlResponse, **kwargs):
-        for next_page_url in _parse_other_page_urls(response):
+
+        for thread_url in _get_thread_rss_urls(response):
+            yield scrapy.Request(thread_url, callback=_get_messages_from_rss)
+
+        for next_page_url in _get_other_page_urls(response):
             yield scrapy.Request(next_page_url, callback=self.parse)
 
 
-def _parse_other_page_urls(response: HtmlResponse) -> List[str]:
+def _get_messages_from_rss(response: XmlResponse, **kwargs) -> List[DhOMessage]:
+    for item in response.xpath('//item'):
+        yield DhOMessage(
+            title=item.xpath('./title/text()').get(),
+        )
+
+
+def _get_thread_rss_urls(response: HtmlResponse) -> List[str]:
+    thread_urls = response.xpath('//a[contains(@role, "menuitem") and contains(@href, "threadId=")]/@href').getall()
+    for thread_url in thread_urls:
+        thread_url.replace('&max=100&', '&max=1000&')
+        yield thread_url
+
+
+def _get_other_page_urls(response: HtmlResponse) -> List[str]:
     next_page_urls = response.xpath(
         '//ul[contains(@class, "pagination")]/li[not(contains(@class, "inactive"))]/a/@href'
     ).getall()
