f2ff9807d008ab32e725c80fc7195b6ef7f99cbb
Björn <bjoernwe@users.noreply.github.com>
2022-11-07 16:16:55 +0100

    Fix import

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from scraper.spiders.DharmaOvergroundSpider import DhOSpider
+from scraper.spiders.DhOSpider import DhOSpider
 
 
 def main():
