1a93c04fd69530b08bae24d1bd69bf7851f92406
Björn <bjoernwe@users.noreply.github.com>
2022-11-08 15:25:25 +0100

    Store parsed messages in file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
+from pathlib import Path
+from typing import Optional
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from scraper.spiders.DhOSpider import DhOSpider, DhOCategory
+from dho_scraper.dho_spider import DhOSpider, DhOCategory
 
 
-def main():
+def main(output_file: Optional[str] = None):
+
+    if output_file:
+        DhOSpider.set_output_feed(jsonlines_path=output_file)
+
     process = CrawlerProcess(get_project_settings())
     process.crawl(DhOSpider, categories=[DhOCategory.DharmaDiagnostics])
     process.start()
 
 
 if __name__ == '__main__':
-    main()
+
+    output_file = './output/items.jsonl'
+    project_root = Path(__file__).parent.parent
+
+    main(output_file=str(project_root.joinpath(output_file)))
