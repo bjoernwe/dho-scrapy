34d28b59bc8534231e0d99854a2a6f96bc9d1ba6
Björn <bjoernwe@users.noreply.github.com>
2023-01-12 19:21:58 +0100

    Add category "Practice Logs"

--- src/dho_scraper/spider.py
+++ src/dho_scraper/spider.py
@@ -10,6 +10,7 @@ from dho_scraper.items import DhOMessage
 class DhOCategory(str, Enum):
     ContemporaryBuddhism = 'https://www.dharmaoverground.org/discussion/-/message_boards/category/13969849'
     DharmaDiagnostics = 'https://www.dharmaoverground.org/discussion/-/message_boards/category/103268'
+    PracticeLogs = 'https://www.dharmaoverground.org/discussion/-/message_boards/category/2658626'
 
 
 class DhOSpider(scrapy.Spider):
