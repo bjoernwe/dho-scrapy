0be656299dabced96d3440ca7988f4560e854755
Björn <bjoernwe@users.noreply.github.com>
2023-01-14 15:07:44 +0100

    Do not change order

--- src/2_convert_data.py
+++ src/2_convert_data.py
@@ -9,9 +9,7 @@ if __name__ == '__main__':
     jsonl_path = data_path.joinpath('messages.jsonl')
     output_path = data_path.joinpath('messages.txt')
 
-    msgs = MessageDB.from_file(jsonl_path=jsonl_path) \
-        .sorted_by_date() \
-        .get_all_messages()
+    msgs = MessageDB.from_file(jsonl_path=jsonl_path).get_all_messages()
     msg_bodies = [msg.msg for msg in msgs]
 
     with open(str(output_path), 'w') as f:
