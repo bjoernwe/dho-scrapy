cffb93458e2e6a75da2c18654886ca879687aa21
Björn <bjoernwe@users.noreply.github.com>
2023-02-04 14:29:28 +0100

    Reformat file according to black

--- src/reddit_api/reddit.py
+++ src/reddit_api/reddit.py
@@ -1,29 +1,27 @@
-PUBLIC = '3heTn63poakqQLY2FJarag'
-SECRET = 'N236hpjVEgeyVOphiBeYbZUVYC4RqA'
+PUBLIC = "3heTn63poakqQLY2FJarag"
+SECRET = "N236hpjVEgeyVOphiBeYbZUVYC4RqA"
 
 
 import requests
 
 auth = requests.auth.HTTPBasicAuth(PUBLIC, SECRET)
 
-subreddit = 'streamentry'
+subreddit = "streamentry"
 url = f"https://oauth.reddit.com/r/{subreddit}/hot"
 
-data = {
-    'grant_type': 'password',
-    'username' : 'CapaceDT',
-    'password': 'SiM9y8I#$E1K'
-}
+data = {"grant_type": "password", "username": "CapaceDT", "password": "SiM9y8I#$E1K"}
 
-headers = { 'User-Agent': 'MyAPI/0.0.1'}
+headers = {"User-Agent": "MyAPI/0.0.1"}
 
-res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+res = requests.post(
+    "https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers
+)
 
-TOKEN = res.json()['access_token']
+TOKEN = res.json()["access_token"]
 
-headers['Authorization'] = f'bearer {TOKEN}'
+headers["Authorization"] = f"bearer {TOKEN}"
 
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers).json()
+requests.get("https://oauth.reddit.com/api/v1/me", headers=headers).json()
 response = requests.get(url, headers=headers)
 data = response.json()
 
