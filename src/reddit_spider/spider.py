d32538d49d487e3be0cb92173064c839202e050b
Björn <bjoernwe@users.noreply.github.com>
2023-02-04 15:43:09 +0100

    Authenticate spider

--- src/reddit_spider/spider.py
+++ src/reddit_spider/spider.py
@@ -1,10 +1,13 @@
 from typing import List
 from typing import Optional
 
+import requests
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import HtmlResponse
 from scrapy.utils.project import get_project_settings
+
+from settings import ScrapySettings
 
 
 class RedditSpider(scrapy.Spider):
@@ -13,14 +16,46 @@ class RedditSpider(scrapy.Spider):
 
     def __init__(self, subreddits: Optional[List[str]] = None, **kwargs):
         super().__init__(**kwargs)
-        # https://docs.scrapy.org/en/latest/intro/tutorial.html
-        # or look at DhOSpider
+        self._subreddits = subreddits or ["streamentry"]
+        self._headers = self._get_auth_headers()
 
     def start_requests(self):
-        pass
+        for subreddit in self._subreddits:
+            url = f"https://oauth.reddit.com/r/{subreddit}/hot"
+            yield scrapy.Request(url=url, callback=self.parse, headers=self._headers)
 
     def parse(self, response: HtmlResponse, **kwargs):
-        pass
+        data = response.json()
+        print(data)
+        # TODO Return a DhOMessage object
+        return data
+
+    @staticmethod
+    def _get_auth_headers() -> dict:
+
+        settings = ScrapySettings()
+
+        auth = requests.auth.HTTPBasicAuth(
+            settings.reddit_public_id, settings.reddit_secret
+        )
+        data = {
+            "grant_type": "password",
+            "username": settings.reddit_username,
+            "password": settings.reddit_password,
+        }
+        headers = {"User-Agent": "RedditSpider/0.0.1"}
+
+        res = requests.post(
+            "https://www.reddit.com/api/v1/access_token",
+            auth=auth,
+            data=data,
+            headers=headers,
+        )
+
+        token = res.json()["access_token"]
+        headers["Authorization"] = f"bearer {token}"
+
+        return headers
 
 
 if __name__ == "__main__":
