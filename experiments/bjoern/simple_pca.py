57f798a4a2431f8c659ce7080caf9c7dce8ab747
Björn <bjoernwe@users.noreply.github.com>
2023-02-05 21:58:06 +0100

    Add PCA experiment

---
+++ experiments/bjoern/simple_pca.py
@@ -0,0 +1,56 @@
+import pickle
+from pathlib import Path
+from textwrap import wrap
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from sklearn.decomposition import PCA
+
+from dho_scraper.categories import DhOCategory
+from message_db.message_db import MessageDB
+
+
+def main():
+
+    # Load messages
+    data_path = Path().resolve().parent.parent.joinpath("data")
+    jsonl_path = data_path.joinpath("messages.jsonl")
+    message_db = MessageDB.from_file(jsonl_path=jsonl_path)
+
+    # Load embeddings
+    embeddings_path = data_path.joinpath("embeddings.pkl")
+    with open(str(embeddings_path), "rb") as f:
+        embedding_db: Dict[int, np.ndarray] = pickle.load(f)
+
+    # Load practice logs of a certain user
+    author_id = "Linda ”Polly Ester” Ö"
+    practice_logs = (
+        message_db.filter_categories(categories={DhOCategory.PracticeLogs})
+        .filter_threads(authors={author_id})
+        .filter_thread_responses(keep_op=True)
+        .sorted_by_date()
+        .get_all_messages()
+    )
+
+    # Calc PCA
+    pca = PCA(n_components=10)
+    embeddings = np.vstack([embedding_db[msg.msg_id] for msg in practice_logs])
+    components = pca.fit_transform(embeddings)
+
+    # Create DataFrame with embeddings and message texts
+    df = pd.DataFrame(
+        components, columns=[f"pca_{i}" for i in range(components.shape[1])]
+    )
+    df["msg_id"] = [msg.msg_id for msg in practice_logs]
+    df["msg"] = ["<br>".join(wrap(msg.msg, width=100)) for msg in practice_logs]
+    print(df)
+
+    # Plot
+    fig = px.scatter(df, x=df.columns[0], y=df.columns[1], hover_data=["msg_id", "msg"])
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
