3ab512a9638211cd42fcb477225534db96a6b50f
Björn <bjoernwe@users.noreply.github.com>
2023-02-08 13:26:00 +0100

    Refactor

--- experiments/calc_embeddings.py
+++ experiments/calc_embeddings.py
@@ -1,6 +1,7 @@
 import pickle
 from pathlib import Path
 from typing import Dict
+from typing import List
 
 import numpy as np
 from sentence_transformers import SentenceTransformer
@@ -9,36 +10,45 @@ from tqdm import tqdm
 from message_db.message_db import MessageDB
 
 
-def calc_embeddings(jsonl_path: Path, out_path: Path):
+def calc_and_store_embeddings(model_names: List[str]):
+
+    data_path = Path(__file__).parent.parent.joinpath("data")
+    jsonl_path = data_path.joinpath("messages.jsonl")
+
+    for model_name in model_names:
+
+        db = MessageDB.from_file(jsonl_path=jsonl_path)
+        msg_embeddings = calc_embeddings(db=db, model_name=model_name)
+
+        out_path = data_path.joinpath(f"models/embeddings_{model_name}.pkl")
+        with open(str(out_path), "wb") as f:
+            pickle.dump(msg_embeddings, f)
+        print(f"Saved: {out_path}")
+
+
+def calc_embeddings(db: MessageDB, model_name: str) -> Dict[int, np.ndarray]:
     """
-    Calculate embeddings (based on the message body) for all messages in jsonl_path and store them in a
+    Calculate embeddings (based on the message body) for all messages in MessageDB and store them in a
     dictionary (msg_id -> embedding-vector).
     """
 
-    db = MessageDB.from_file(jsonl_path=jsonl_path)
-    model = SentenceTransformer("all-MiniLM-L12-v2")
-
+    model = SentenceTransformer(model_name)
     msg_embeddings: Dict[int, np.ndarray] = {}
 
     print(f"Calculating embeddings for {len(db)} messages ...")
 
     for msg in tqdm(db.get_all_messages()):
         msg_emb = model.encode([msg.msg])
-        assert msg_emb.shape == (1, 384)
         msg_embeddings[msg.msg_id] = msg_emb
 
-    with open(str(out_path), "wb") as f:
-        pickle.dump(msg_embeddings, f)
-    print(f"Saved: {out_path}")
+    return msg_embeddings
 
 
 if __name__ == "__main__":
 
-    # model_name = "all-MiniLM-L12-v2"
-    model_name = "multi-qa-mpnet-base-dot-v1"
+    model_names = [
+        "all-MiniLM-L12-v2",
+        "multi-qa-mpnet-base-dot-v1",
+    ]
 
-    data_path = Path(__file__).parent.parent.joinpath("data")
-    jsonl_path = data_path.joinpath("messages.jsonl")
-    out_path = data_path.joinpath(f"embeddings_{model_name}.pkl")
-
-    calc_embeddings(jsonl_path=jsonl_path, out_path=out_path)
+    calc_and_store_embeddings(model_names=model_names)
