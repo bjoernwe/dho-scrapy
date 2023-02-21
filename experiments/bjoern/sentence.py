e576fe58b627caba71b487560b016a38a29e1ab4
Björn <bjoernwe@users.noreply.github.com>
2023-02-21 14:26:39 +0100

    Add sentence index

--- experiments/bjoern/sentence.py
+++ experiments/bjoern/sentence.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 class Sentence(BaseModel):
 
     msg_id: int
+    sentence_idx: int
     sentence: str
 
     @property
     def sid(self) -> str:
         key = (
             str(self.msg_id).encode("UTF-8"),
+            str(self.sentence_idx).encode("UTF-8"),
             self.sentence.encode("UTF-8"),
         )
         sha = hashlib.sha1()
