4b5a5b286dc03f520e27faf5e2943c53b5c498ce
Björn <bjoernwe@users.noreply.github.com>
2023-02-24 19:40:21 +0100

    Fix sentence ID

--- data_models/sentence.py
+++ data_models/sentence.py
@@ -14,4 +14,12 @@ class Sentence(BaseModel):
 
     @property
     def sid(self) -> str:
-        return str(self.__hash__())
+        key = (
+            str(self.msg_id).encode("UTF-8"),
+            str(self.sentence_idx).encode("UTF-8"),
+            self.sentence.encode("UTF-8"),
+        )
+        sha = hashlib.sha1()
+        for k in key:
+            sha.update(k)
+        return sha.hexdigest()[:10]
