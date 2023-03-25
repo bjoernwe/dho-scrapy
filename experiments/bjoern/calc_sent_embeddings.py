b4c55c9c3be4b26e10af5430c25fcd614908b674
Björn <bjoernwe@users.noreply.github.com>
2023-03-25 21:10:33 +0100

    Parameterize window size

--- experiments/bjoern/calc_sent_embeddings.py
+++ experiments/bjoern/calc_sent_embeddings.py
@@ -6,30 +6,37 @@ from experiments.utils.paths import default_embeddings_path
 from experiments.utils.paths import default_jsonl_path
 
 
-def main():
+def main(
+    jsonl_path: Path = default_jsonl_path,
+    model_name: str = "paraphrase-albert-small-v2",
+    window_size: int = 1,
+):
 
-    model_name = "paraphrase-albert-small-v2"
-    message_db = MessageDB.from_file(jsonl_path=default_jsonl_path)
+    message_db = MessageDB.from_file(jsonl_path=jsonl_path)
     embeddings_shelve_path = default_embeddings_path.joinpath(
-        f"sentences_1_{model_name}.shelve"
+        f"sentences_{window_size}_{model_name}.shelf"
     )
 
     calc_sentence_embeddings(
         model_name=model_name,
         message_db=message_db,
         embeddings_shelve_path=embeddings_shelve_path,
+        window_size=window_size,
     )
 
 
 def calc_sentence_embeddings(
-    model_name: str, message_db: MessageDB, embeddings_shelve_path: Path
+    model_name: str,
+    message_db: MessageDB,
+    embeddings_shelve_path: Path,
+    window_size: int,
 ):
 
     embedding_db = EmbeddingsDB(
         model_name=model_name, shelf_path=embeddings_shelve_path
     )
 
-    sentences = message_db.get_sentences()
+    sentences = message_db.get_sentences(window_size=window_size)
     embedding_db.add_sentences(sentences=sentences)
 
 
