2b66a9453f630c046c5a493960f73141cdf6047f
Björn <bjoernwe@users.noreply.github.com>
2023-03-26 20:33:34 +0200

    Simplify embedding

--- data_tools/default_paths.py
+++ data_tools/default_paths.py
@@ -5,3 +5,9 @@ project_path = Path(__file__).parent.parent
 data_path = project_path.joinpath("data")
 default_cache_path = data_path.joinpath("cache")
 default_jsonl_path = data_path.joinpath("messages.jsonl")
+
+
+def get_default_cache_path(sentences_per_snippet: int, model_name: str) -> Path:
+    return default_cache_path.joinpath(
+        f"emb_snt_{sentences_per_snippet}_{model_name}.shelf"
+    )
