889efffecb82084b8f09fb4260927a8d9e6b7fbe
Superb Owl <owl@superb-owl.link>
2023-04-22 16:59:16 -0400

    can be run sequentially

--- scraper/reddit/main.py
+++ scraper/reddit/main.py
@@ -1,16 +1,12 @@
 import os
 import argparse
-import praw
+import requests
 import psycopg2
 from datetime import datetime
 from psycopg2 import Error
 
-# Load Reddit credentials from environment variables
-REDDIT_CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
-REDDIT_CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
-REDDIT_USER_AGENT = os.environ.get('REDDIT_USER_AGENT')
-REDDIT_USERNAME = os.environ.get('REDDIT_USERNAME')
-REDDIT_PASSWORD = os.environ.get('REDDIT_PASSWORD')
+# Load Pushshift API base URL from environment variables
+PUSHSHIFT_API_URL = "https://api.pushshift.io/reddit/search/submission/"
 
 # Load PostgreSQL details from environment variables
 POSTGRES_USER = os.environ.get('PGUSER')
@@ -18,13 +14,6 @@ POSTGRES_PASSWORD = os.environ.get('PGPASSWORD')
 POSTGRES_HOST = os.environ.get('PGHOST')
 POSTGRES_PORT = os.environ.get('PGPORT')
 POSTGRES_DATABASE = os.environ.get('PGDATABASE')
-
-# Initialize the Reddit API wrapper
-reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
-                     client_secret=REDDIT_CLIENT_SECRET,
-                     user_agent=REDDIT_USER_AGENT,
-                     username=REDDIT_USERNAME,
-                     password=REDDIT_PASSWORD)
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -34,42 +23,62 @@ args = parser.parse_args()
 # Connect to PostgreSQL database
 def connect():
     return psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        dbname=db_name,
-        user=db_user,
-        password=db_password
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+        dbname=POSTGRES_DATABASE,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD
     )
 
-# Connect to the PostgreSQL database
-def connect():
-    return psycopg2.connect(user=POSTGRES_USER,
-                                  password=POSTGRES_PASSWORD,
-                                  host=POSTGRES_HOST,
-                                  port=POSTGRES_PORT,
-                                  database=POSTGRES_DATABASE)
+def retrieve_min_created(subreddit):
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("SELECT MIN(created_utc) from posts WHERE subreddit = %s;", (subreddit,))
+    min_created_utc = cursor.fetchone()[0]
+    connection.close()
+    return min_created_utc
+
+def get_posts(subreddit, limit=None, before=None):
+    params = {
+        "subreddit": subreddit,
+        "size": limit or 100,
+        "sort_type": "created_utc"
+    }
+    if before is not None:
+        print(before)
+        ago = datetime.now() - before
+        params["before"] = "%ds" % ago.total_seconds()
+    response = requests.get(PUSHSHIFT_API_URL, params=params)
+    if response.status_code == 200:
+        return response.json()["data"]
+    else:
+        print(f"Error {response.status_code} while fetching from Pushshift API")
+        print(response.json())
+        return []
 
 # Define the subreddit to scrape
-subreddit = reddit.subreddit(args.subreddit)
+subreddit = args.subreddit
+
+# Retrieve min created_utc for the given subreddit from postgres
+min_created_utc = retrieve_min_created(subreddit)
+
+# Get the posts from the Pushshift API
+posts = get_posts(subreddit, before=min_created_utc)
 
 # Iterate through all the posts in the subreddit
-for post in subreddit.new(limit=None):
-    created = datetime.fromtimestamp(post.created_utc)
+for post in posts:
+    created = datetime.fromtimestamp(post["created_utc"])
 
     try:
         # Insert the post data into the database
         connection = connect()
         cursor = connection.cursor()
         cursor.execute("""
-            INSERT INTO posts (id, title, author, created_utc, num_comments, score, url, text)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
-        """, (post.id, post.title, post.author.name, created, post.num_comments, post.score, post.url, post.selftext))
+            INSERT INTO posts (id, subreddit, title, author, created_utc, num_comments, score, url, text)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """, (post["id"], subreddit, post["title"], post["author"], created, post["num_comments"], post["score"], post["url"], post["selftext"]))
         connection.commit()
         print("Post inserted successfully")
     except (Exception, Error) as error:
         print("Error while inserting post into PostgreSQL database:", error)
 
-# Close the database connection
-cursor.close()
-connection.close()
-print("PostgreSQL connection is closed")
